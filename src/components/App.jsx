ff396f0a6daded45fca142f03c18181fba08a075
Evgenija26 <98258381+Evgenija26@users.noreply.github.com>
2023-01-24 15:12:18 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,10 @@ export const App = () => {
       style={{
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
+        // justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
